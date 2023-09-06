555c52a6b33db4debd6a9a8ea6616ea7ef26536f
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-09-06 16:11:53 +0200

    move code coverage flags out of intel.cmake file, and out of waq_flags and into general flags

--- src/cmake/compiler_options/intel.cmake
+++ src/cmake/compiler_options/intel.cmake
@@ -74,11 +74,3 @@ endif(UNIX)
 
 set(qauto_threaded_flags ${automatic_local_variable_storage_flag} ${generate_reentrancy_threaded_flag})
 set(waq_default_flags ${file_preprocessor_flag} ${extend_source132_flag} ${traceback_flag})
-
-
-# Define the custom flag about code coverage with a default value of OFF
-option(ENABLE_CODE_COVERAGE "Enable the code and profiling coverage" OFF)
-if(ENABLE_CODE_COVERAGE)
-    message("Code coverage and profiling analysis is enabled")
-    set(waq_default_flags ${waq_default_flags} ${codecov_flag} ${profiling_flag})
-endif(ENABLE_CODE_COVERAGE)
