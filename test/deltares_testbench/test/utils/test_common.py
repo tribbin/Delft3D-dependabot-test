99ff18d720eaf46b1f13fa76bc5b4538e18b88c7
Matthijs Huisman <matthijs.huisman@deltares.nl>
2023-11-08 11:19:35 +0100

    Add testcase and fix for numpy floating point numbers to print with the formatted syntax

--- test/deltares_testbench/test/utils/test_common.py
+++ test/deltares_testbench/test/utils/test_common.py
@@ -1,3 +1,4 @@
+import numpy as np
 from unittest.mock import Mock, patch, call
 from src.utils.logging.log_level import LogLevel
 from src.utils.common import log_header, log_separator, log_table
@@ -62,33 +63,33 @@ class TestCommon:
         logger_instance.log.assert_has_calls(expected_calls)
 
     @patch('src.utils.logging.console_logger.ConsoleLogger')
-    def test_log_table_real_example(self, mock_logger):
+    def test_convert_numpy_float_to_python_float(self, mock_logger):
         # Arrange
         logger_instance = Mock()
         mock_logger.return_value = logger_instance
 
         sample_table = {
-            "Test case name": ["e02_f029_c501_junction-advection-acceleration-equidistant_WAQ"],
-            "Runtime": ["1.177e+00"],
-            "Ratio": ["1.177e+00"],
-            "Result": ["OK"],
-            "MaxAbsDiff": ["8.881784197001252e-16"],
-            "MaxRelDiff": ["1.5340265946406362e-08"],
-            "File name": ["DFM_DELWAQ_Water_Quality/deltashell_map.nc"],
-            "Parameter name": ["mesh1d_"],
-            "Information": ["at coordinates()"],
+            "Test name": [],
+            "Runtime": [],
+            "MaxAbsDiff": [],
+            "File name": [],
         }
+
+        sample_table["Test name"].append("name_1")
+        sample_table["Runtime"].append("1.177e+00")
+        sample_table["MaxAbsDiff"].append(np.float64(8.881784197001252e-16))
+        sample_table["File name"].append("abc.nc")
 
         # Act
         log_table(sample_table, logger_instance, LogLevel.INFO, "-")
 
         # Assert
         expected_calls = [
-            call('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------', LogLevel.INFO),
-            call('|Test case name                                               |Runtime  |Ratio    |Result|MaxAbsDiff           |MaxRelDiff            |File name                                 |Parameter name|Information     |', LogLevel.INFO),
-            call('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------', LogLevel.INFO),
-            call('|e02_f029_c501_junction-advection-acceleration-equidistant_WAQ|1.177e+00|1.177e+00|OK    |8.881784197001252e-16|1.5340265946406362e-08|DFM_DELWAQ_Water_Quality/deltashell_map.nc|mesh1d_       |at coordinates()|', LogLevel.INFO),
-            call('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------', LogLevel.INFO),
+            call('------------------------------------------', LogLevel.INFO),
+            call('|Test name|Runtime  |MaxAbsDiff|File name|', LogLevel.INFO),
+            call('------------------------------------------', LogLevel.INFO),
+            call('|name_1   |1.177e+00|8.882e-16 |abc.nc   |', LogLevel.INFO),
+            call('------------------------------------------', LogLevel.INFO),
         ]
 
         logger_instance.log.assert_has_calls(expected_calls)
