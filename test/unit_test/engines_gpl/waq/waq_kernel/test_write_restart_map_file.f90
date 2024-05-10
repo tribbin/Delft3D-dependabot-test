b8950d14bb2427a9151f77095877f5bd6fca187a
Mostafa Farrag <moah.farag@gmail.com>
2024-05-10 14:26:24 +0200

    rename test_dlwq13 to test_write_restart_map_file

--- test/unit_test/engines_gpl/waq/waq_kernel/test_write_restart_map_file.f90
+++ test/unit_test/engines_gpl/waq/waq_kernel/test_write_restart_map_file.f90
@@ -21,13 +21,13 @@
 !!  of Stichting Deltares remain the property of Stichting Deltares. All
 !!  rights reserved.
 
-program test_dlwq13
-    !! tests_dlwq13.f90 --
+program test_write_restart_map_file
+    !! tests_write_restart_map_file.f90 --
     !! Run unit tests for routines in Delft3D-WAQ
 
-    use ftnunit, only : runtests_init, runtests, runtests_final, assert_true, assert_files_comparable, test, &
+    use ftnunit, only: runtests_init, runtests, runtests_final, assert_true, assert_files_comparable, test, &
             prepare_tests
-    use m_write_restart_map_file, only : write_restart_map_file
+    use m_write_restart_map_file, only: write_restart_map_file
 
     implicit none
     character(len = 200) :: cmd_arg
@@ -44,21 +44,21 @@ program test_dlwq13
         call get_command_argument(1, cmd_arg)
 
         select case (trim(cmd_arg))
-        case('test_dlwq13_recognise_nans')
-            write(*, *) "Running test_dlwq13_recognise_nans"
-            call runtests(call_test_dlwq13_recognise_nans)
-        case ('test_dlwq13_no_nans')
-            write(*, *) "Running test_dlwq13_no_nans"
-            call runtests(call_test_dlwq13_no_nans)
-        case ('test_dlwq13_with_nans')
-            write(*, *) "Running test_dlwq13_with_nans"
-            call runtests(call_test_dlwq13_with_nans)
+        case('test_write_restart_map_file_recognise_nans')
+            write(*, *) "Running test_write_restart_map_file_recognise_nans"
+            call runtests(call_test_write_restart_map_file_recognise_nans)
+        case ('test_write_restart_map_file_no_nans')
+            write(*, *) "Running test_write_restart_map_file_no_nans"
+            call runtests(call_test_write_restart_map_file_no_nans)
+        case ('test_write_restart_map_file_with_nans')
+            write(*, *) "Running test_write_restart_map_file_with_nans"
+            call runtests(call_test_write_restart_map_file_with_nans)
         end select
     else
         write(*, *) "No test specified, running all tests"
-        call runtests(call_test_dlwq13_recognise_nans)
-        call runtests(call_test_dlwq13_no_nans)
-        call runtests(call_test_dlwq13_with_nans)
+        call runtests(call_test_write_restart_map_file_recognise_nans)
+        call runtests(call_test_write_restart_map_file_no_nans)
+        call runtests(call_test_write_restart_map_file_with_nans)
     end if
 
     call runtests_final
@@ -66,20 +66,20 @@ program test_dlwq13
 
 contains
 
-    subroutine call_test_dlwq13_recognise_nans
-        call test(test_dlwq13_recognise_nans, 'Recognising NaNs')
-    end subroutine call_test_dlwq13_recognise_nans
+    subroutine call_test_write_restart_map_file_recognise_nans
+        call test(test_write_restart_map_file_recognise_nans, 'Recognising NaNs')
+    end subroutine call_test_write_restart_map_file_recognise_nans
 
-    subroutine call_test_dlwq13_no_nans
-        call test(test_dlwq13_no_nans, 'write_restart_map_file: final result without NaNs')
-    end subroutine call_test_dlwq13_no_nans
+    subroutine call_test_write_restart_map_file_no_nans
+        call test(test_write_restart_map_file_no_nans, 'write_restart_map_file: final result without NaNs')
+    end subroutine call_test_write_restart_map_file_no_nans
 
-    subroutine call_test_dlwq13_with_nans
-        call test(test_dlwq13_with_nans, 'write_restart_map_file: final result with NaNs')
-    end subroutine call_test_dlwq13_with_nans
+    subroutine call_test_write_restart_map_file_with_nans
+        call test(test_write_restart_map_file_with_nans, 'write_restart_map_file: final result with NaNs')
+    end subroutine call_test_write_restart_map_file_with_nans
 
-    subroutine test_dlwq13_recognise_nans
-        ! test_dlwq13_recognise_nans --
+    subroutine test_write_restart_map_file_recognise_nans
+        ! test_write_restart_map_file_recognise_nans --
         !     Unit test for recognising NaNs (interference from optimiser?)
 
         real :: x, y
@@ -89,10 +89,10 @@ contains
 
         call assert_true(x /= x, 'X recognised as NaN')
 
-    end subroutine test_dlwq13_recognise_nans
+    end subroutine test_write_restart_map_file_recognise_nans
 
-    subroutine test_dlwq13_no_nans
-        ! test_dlwq13_no_nans --
+    subroutine test_write_restart_map_file_no_nans
+        ! test_write_restart_map_file_no_nans --
         !     Unit test for write_restart_map_file (write restart file)
         ! Note:
         !     There should be no error message
@@ -112,9 +112,9 @@ contains
         ! Get the DATA_PATH environment variable
         call get_environment_variable("DATA_PATH", dataPath)
 
-        file_name_list(18) = trim(dataPath) // '/test_dlwq13_no_nans.ref' ! Not used in write_restart_map_file
-        file_name_list(19) = trim(dataPath) // '/test_dlwq13_no_nans.mon'
-        file_name_list(23) = trim(dataPath) // '/test_dlwq13_no_nans.res'
+        file_name_list(18) = trim(dataPath) // '/test_write_restart_map_file_no_nans.ref' ! Not used in write_restart_map_file
+        file_name_list(19) = trim(dataPath) // '/test_write_restart_map_file_no_nans.mon'
+        file_name_list(23) = trim(dataPath) // '/test_write_restart_map_file_no_nans.res'
 
         open (newunit = lun(19), file = file_name_list(19))
 
@@ -127,10 +127,10 @@ contains
         call assert_files_comparable(file_name_list(19), file_name_list(18), 'Monitor file contains no messages', 1.0e-7)
         call assert_true(all(conc == 1.0), 'Concentration array unchanged')
 
-    end subroutine test_dlwq13_no_nans
+    end subroutine test_write_restart_map_file_no_nans
 
-    subroutine test_dlwq13_with_nans
-        ! test_dlwq13_with_nans --
+    subroutine test_write_restart_map_file_with_nans
+        ! test_write_restart_map_file_with_nans --
         !     Unit test for write_restart_map_file (write restart file)
         !
         ! Note:
@@ -143,9 +143,9 @@ contains
         integer, dimension(30) :: lun
         character(len = 200) :: dataPath
         character(len = 255), dimension(30) :: file_name_list
-        character(len = 40), dimension(4)   :: mname
-        character(len = 20), dimension(10)  :: sname
-        real                                :: x
+        character(len = 40), dimension(4) :: mname
+        character(len = 20), dimension(10) :: sname
+        real :: x
 
         conc = 1.0
         x = -1.0
@@ -154,9 +154,9 @@ contains
         ! Get the DATA_PATH environment variable
         call get_environment_variable("DATA_PATH", dataPath)
 
-        file_name_list(18) = trim(dataPath) // '/test_dlwq13_with_nans.ref' ! Not used in write_restart_map_file
-        file_name_list(19) = trim(dataPath) // '/test_dlwq13_with_nans.mon'
-        file_name_list(23) = trim(dataPath) // '/test_dlwq13_with_nans.res'
+        file_name_list(18) = trim(dataPath) // '/test_write_restart_map_file_with_nans.ref' ! Not used in write_restart_map_file
+        file_name_list(19) = trim(dataPath) // '/test_write_restart_map_file_with_nans.mon'
+        file_name_list(23) = trim(dataPath) // '/test_write_restart_map_file_with_nans.res'
 
         open (newunit = lun(19), file = file_name_list(19))
 
@@ -169,6 +169,6 @@ contains
         call assert_files_comparable(file_name_list(19), file_name_list(18), 'Monitor file contains no messages', 1.0e-7)
         call assert_true(any(conc == 0.0), 'NaNs in concentration array replaced by 0')
 
-    end subroutine test_dlwq13_with_nans
+    end subroutine test_write_restart_map_file_with_nans
 
-end program test_dlwq13
+end program test_write_restart_map_file
