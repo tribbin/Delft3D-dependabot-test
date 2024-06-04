ee25e592fd0987a0d54872e5047bafb7c71a6c98
noback <Matthias.Noback@deltares.nl>
2024-06-04 14:41:07 +0200

    Upgrade script to allow parallel run

--- tools/fprettify/run_fprettify.sh
+++ tools/fprettify/run_fprettify.sh
@@ -1,8 +1,16 @@
 #!/bin/env bash
 
-fortran_files=$(find "${1:-.}" -type f -iname "*.f90")
+dir="${1:-./}"
+if [[ ! $dir =~ ^"./" ]]; then
+    dir="./$dir"
+fi
 
-for fortran_file in ${fortran_files}; do
-    echo "Converting ${fortran_file}"
-    fprettify --config-file ./tools/fprettify/.fprettify.rc ${fortran_file}
-done
+echo "Look for Fortran files in directory: $dir"
+
+cores_available=$(nproc)
+cores="${2:-${cores_available}}"
+
+echo "Using $cores cores"
+
+find "$dir" -type f -iname "*.f90" -not \( -path "./src/third_party/*" -o -path "./src/third_party_open/*" -o -path "./src/engines_gpl/dflowfm/packages/dflowfm_lib/templates/*" \) \
+    | xargs --max-args 10 --max-procs $(nproc) timeout --foreground -v 10 fprettify --config-file tools/fprettify/.fprettify.rc
