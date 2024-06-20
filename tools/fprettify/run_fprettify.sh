05250f92fcf0974bda81e859093d5d183df25611
noback <Matthias.Noback@deltares.nl>
2024-06-20 16:34:16 +0200

    Remove saadf.f90 from excluded list

--- tools/fprettify/run_fprettify.sh
+++ tools/fprettify/run_fprettify.sh
@@ -12,5 +12,5 @@ cores="${2:-${cores_available}}"
 
 echo "Using $cores cores"
 
-find "$dir" -type f -iname "*.f90" -not \( -path "./src/third_party/*" -o -path "./src/third_party_open/*" -o -path "./src/engines_gpl/dflowfm/packages/dflowfm_lib/templates/*" -o -path "./src/engines_gpl/flow2d3d/*" -o -path "./src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_utils/saadf.F90" \) \
+find "$dir" -type f -iname "*.f90" -not \( -path "./src/third_party/*" -o -path "./src/third_party_open/*" -o -path "./src/engines_gpl/dflowfm/packages/dflowfm_lib/templates/*" -o -path "./src/engines_gpl/flow2d3d/*" \) \
     | xargs --max-args 1 --max-procs $(nproc) timeout --foreground -v 10 fprettify --config-file tools/fprettify/.fprettify.rc
