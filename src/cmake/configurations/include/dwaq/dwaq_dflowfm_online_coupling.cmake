5ecc7a71c412954f58638ab508ec93300f62898f
Hidde Elzinga <hidde.elzinga@deltares.nl>
2024-07-04 07:49:30 +0000

    DELWAQ-853 Made a separate project for waq_proc_preprocess

--- src/cmake/configurations/include/dwaq/dwaq_dflowfm_online_coupling.cmake
+++ src/cmake/configurations/include/dwaq/dwaq_dflowfm_online_coupling.cmake
@@ -3,3 +3,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/dwaq_functions.cmake)
 
 add_target_with_subdirectory(wq_processes ${wq_processes_module})
+add_target_with_subdirectory(waq_proc_preprocess ${waq_proc_preprocess_module})
