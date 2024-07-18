c68666b1023313e191a37dac50e71b8fa2c88ce5
FlorisBuwaldaDeltares <117371974+FlorisBuwaldaDeltares@users.noreply.github.com>
2024-07-18 11:27:25 +0200

    GPL -> AGPL license

--- src/engines_gpl/rr/packages/rr_kernel_f/src/nodes_from_flow1d.f90
+++ src/engines_gpl/rr/packages/rr_kernel_f/src/nodes_from_flow1d.f90
@@ -1,17 +1,17 @@
-!----- GPL ---------------------------------------------------------------------
+!----- AGPL ---------------------------------------------------------------------
 !                                                                               
 !  Copyright (C)  Stichting Deltares, 2011-2024.                                
 !                                                                               
 !  This program is free software: you can redistribute it and/or modify         
-!  it under the terms of the GNU General Public License as published by         
-!  the Free Software Foundation version 3.                                      
+!  it under the terms of the GNU Affero General Public License as               
+!  published by the Free Software Foundation version 3.                         
 !                                                                               
 !  This program is distributed in the hope that it will be useful,              
 !  but WITHOUT ANY WARRANTY; without even the implied warranty of               
 !  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                
-!  GNU General Public License for more details.                                 
+!  GNU Affero General Public License for more details.                          
 !                                                                               
-!  You should have received a copy of the GNU General Public License            
+!  You should have received a copy of the GNU Affero General Public License     
 !  along with this program.  If not, see <http://www.gnu.org/licenses/>.        
 !                                                                               
 !  contact: delft3d.support@deltares.nl                                         
@@ -31,7 +31,7 @@
 !!  Copyright (C)  Stichting Deltares, 2017-2019.
 !!
 !!  This program is free software: you can redistribute it and/or modify
-!!  it under the terms of the GNU Affero General Public License as
+!!  it under the terms of the GNU Affero General Public License as      
 !!  published by the Free Software Foundation version 3.
 !!
 !!  This program is distributed in the hope that it will be useful,
@@ -39,7 +39,7 @@
 !!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 !!  GNU Affero General Public License for more details.
 !!
-!!  You should have received a copy of the GNU Affero General Public License
+!!  You should have received a copy of the GNU Affero General Public License     
 !!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 !!
 !!  contact: delft3d.support@deltares.nl
