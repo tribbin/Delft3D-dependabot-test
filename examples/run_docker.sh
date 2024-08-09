21912e90ea9706a8e29ed774f9bd51771a13b752
Matthijs Huisman <matthijs.huisman@deltares.nl>
2024-08-09 16:10:52 +0200

    Update run docker with image from harbor

--- examples/run_docker.sh
+++ examples/run_docker.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# List all subfolders in the current directory
+IMAGE="containers.deltares.nl/delft3d/legacy/delft3dfm:latest"
+
+# Pull the Docker image
+docker pull $IMAGE
+
+# List all subfolders in the current directory and run the script if it exists
 for dir in */; do
     echo "Checking folder: $dir"
-    cd "$dir"
-    if [ -f run_docker.sh ]; then
-        echo "Running run_docker.sh in $dir"
-        ./run_docker.sh
+    if [ -f "$dir/run_docker.sh" ]; then
+		cd $dir
+        echo "Running run_docker.sh in $(pwd)"
+        docker run -v ".:/data" $IMAGE
+		cd ..
     else
-        echo "No run script in $dir"
+        echo "No run script in $(pwd)"
     fi
-    cd ..
 done
