653e50dedf28e093d31ae9de889347f0074a7366
Matthijs Huisman <matthijs.huisman@deltares.nl>
2024-08-23 10:26:40 +0200

    add FI_PROVIDER_PATH env to docker run

--- examples/dflowfm/run-all-examples-docker.sh
+++ examples/dflowfm/run-all-examples-docker.sh
@@ -3,8 +3,8 @@
 IMAGE="containers.deltares.nl/delft3d/legacy/delft3dfm:latest"
 
 MPI_DIR=/opt/apps/intelmpi/2021.10.0/mpi/2021.10.0
-container_PATH=$MPI_DIR/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/intel/mpi/bin:/opt/intel/mpi/libfabric/bin
-container_LD_LIBRARY_PATH=$MPI_DIR/lib:$MPI_DIR/lib/release:/opt/intel/mpi/lib:/opt/intel/mpi/libfabric/lib
+PATH=$MPI_DIR/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/intel/mpi/bin:/opt/intel/mpi/libfabric/bin
+LD_LIBRARY_PATH=$MPI_DIR/lib:$MPI_DIR/lib/release:/opt/intel/mpi/lib:/opt/intel/mpi/libfabric/lib
 
 # Pull the Docker image
 docker pull $IMAGE
@@ -19,8 +19,9 @@ for dir in */; do
 			-v ".:/data" \
 			-v "$MPI_DIR:$MPI_DIR" \
 			-v "/usr/:/host" \
-			-e PATH=$container_PATH \
-			-e LD_LIBRARY_PATH=$container_LD_LIBRARY_PATH \
+			-e PATH=$PATH \
+			-e LD_LIBRARY_PATH=$LD_LIBRARY_PATH \
+			-e FI_PROVIDER_PATH=/opt/intel/mpi/libfabric/lib/prov \
 			--shm-size 8G \
 			$IMAGE
 		echo "##teamcity[testFinished name='$dir']"
