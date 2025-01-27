23018d7df35cdc711a1a6ac0eeba1edc62a20a3c
Robin van Westrenen <robin.vanwestrenen@deltares.nl>
2025-01-27 15:44:01 +0000

    DEVOPSDSC-449: #4 Pipeline with build-on-demand

--- ci/teamcity/Delft3D/linux/build.kt
+++ ci/teamcity/Delft3D/linux/build.kt
@@ -17,23 +17,22 @@ object LinuxBuild : BuildType({
     )
 
     name = "Build"
-    buildNumberPattern = "%build.vcs.number%"
+    buildNumberPattern = "%product%: %build.vcs.number%"
     description = "Linux build."
 
     allowExternalStatus = true
     artifactRules = """
         #teamcity:symbolicLinks=as-is
         **/*.log => logging
-        %install_dir%/** => oss_artifacts_lnx64_%build.vcs.number%.tar.gz!lnx64
+        build_%product%/install/** => oss_artifacts_lnx64_%build.vcs.number%.tar.gz!lnx64
     """.trimIndent()
 
     params {
         param("intel_oneapi_version", "2023")
         param("intel_fortran_compiler", "ifort")
         param("generator", """"Unix Makefiles"""")
-        param("install_dir", "build_all/install")
         param("build_type", "Release")
-        param("build_configuration", "all")
+        select("product", "auto-select", display = ParameterDisplay.PROMPT, options = listOf("auto-select", "all-testbench", "fm-suite", "d3d4-suite", "fm-testbench", "d3d4-testbench", "waq-testbench", "part-testbench", "rr-testbench", "wave-testbench", "swan-testbench"))
     }
 
     vcs {
@@ -61,10 +60,11 @@ object LinuxBuild : BuildType({
                 . /opt/intel/oneapi/setvars.sh
                 export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64:${'$'}{LD_LIBRARY_PATH}
                 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig:${'$'}{PKG_CONFIG_PATH}
-                export FC=mpi%intel_fortran_compiler% CXX=mpicxx CC=mpiicx                
-                cmake ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%build_configuration% -D CMAKE_BUILD_TYPE=%build_type% -B build_%build_configuration% -D CMAKE_INSTALL_PREFIX=%install_dir%
+                export FC=mpi%intel_fortran_compiler% CXX=mpicxx CC=mpiicx
                 
-                cd build_%build_configuration%
+                cmake ./src/cmake -G %generator% -D CONFIGURATION_TYPE:STRING=%product% -D CMAKE_BUILD_TYPE=%build_type% -B build_%product% -D CMAKE_INSTALL_PREFIX=build_%product%/install
+                
+                cd build_%product%
                 cmake --build . -j --target install --config %build_type%
             """.trimIndent()
             dockerImage = "containers.deltares.nl/delft3d-dev/delft3d-third-party-libs:oneapi-%intel_oneapi_version%-%intel_fortran_compiler%-release"
@@ -84,9 +84,9 @@ object LinuxBuild : BuildType({
                 LIBESMF=`ldd ${'$'}{ESMFRWG} | grep libesmf.so | awk '{print ${'$'}3}'`
                 LIBCILKRTS=`ldd ${'$'}{ESMFRWG} | grep libcilkrts.so | awk '{print ${'$'}3}'`
                 
-                cp -rf ${'$'}{ESMFRWG}                                         %install_dir%/bin                               &>/dev/null
-                cp -rf ${'$'}{LIBESMF}                                         %install_dir%/lib                               &>/dev/null
-                cp -rf ${'$'}{LIBCILKRTS}                                      %install_dir%/lib                               &>/dev/null
+                cp -rf ${'$'}{ESMFRWG}    build_%product%/install/bin &>/dev/null
+                cp -rf ${'$'}{LIBESMF}    build_%product%/install/lib &>/dev/null
+                cp -rf ${'$'}{LIBCILKRTS} build_%product%/install/lib &>/dev/null
             """.trimIndent()
         }
     }
