dfe8bdd6ee249cdadd60a566476ac56cc23e509e
buwalda <floris.buwalda@deltares.nl>
2025-03-11 11:21:39 +0100

    fix linux configuration, remove obsolete .dll's from suite binary json

--- src/cmake/configurations/components/fbc_configuration.cmake
+++ src/cmake/configurations/components/fbc_configuration.cmake
@@ -3,7 +3,7 @@ if(NOT TARGET fbc-tools)
 endif()
 if(UNIX)
   # install
-  add_subdirectory(${checkout_src_root}/${install_d_hydro_module} install_d_hydro)
+  add_subdirectory(${checkout_src_root}/${install_fbc_module} install_fbc-tools)
 endif()
 # Project name must be at the end of the configuration: it might get a name when including other configurations and needs to overwrite that
 project(fbc-tools)
