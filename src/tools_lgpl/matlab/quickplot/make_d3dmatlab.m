7a4eac07eea34d40ec43f606214a3732f8f263c4
hrajagers <hrajagers@gmail.com>
2025-03-12 23:43:43 +0100

    improve the build steps to fail whenever the version number contains the text "(changed)"

--- src/tools_lgpl/matlab/quickplot/make_d3dmatlab.m
+++ src/tools_lgpl/matlab/quickplot/make_d3dmatlab.m
@@ -119,7 +119,10 @@ if ~exist('netcdf/snctools','dir')
 end
 
 % strip off the platform flag (binaries for Windows and Linux are included)
-qpversion = deblank(sscanf(qpversion,'%[^(]'));
+% ... but don't strip of (changed)
+% ... the platform is the last string between brackets
+brackets = strfind(qpversion,'(');
+qpversion = qpversion(1:max(brackets)-2);
 % for the progress statement add the platform statement
 qpversion_ = [qpversion, ' (all platforms)'];
 
