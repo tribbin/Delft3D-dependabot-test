523229149b95471f5265c357f83b6f566c1717ee
Willem Ottevanger <willem.ottevanger@deltares.nl>
2025-04-02 15:47:10 +0200

    use comparereal

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/setfixedweirs.f90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/prepost/setfixedweirs.f90
@@ -66,7 +66,8 @@ contains
       use m_wall_clock_time
       use m_find_crossed_links_kdtree2
       use m_filez, only: oldfil, doclose, newfil
-
+      use precision_basics, only: comparereal
+      
       integer :: k, kk, n1, n2, n, L, LL, jacros, minp, kint, ierr, nh, nhh, i, Lf
       integer :: jaweir, Lastfoundk, kf, kL, Lnt, nna, nnb, k3, k4
       integer, allocatable :: ihu(:)
@@ -284,7 +285,7 @@ contains
 
          ! if ( (zc > bobL .and. zc > zcrest(L)) .or. ( (ifixedweirscheme == 8 .or. ifixedweirscheme == 9) .and. ifirstweir(L) == 1) ) then   ! For Villemonte and Tabellenboek fixed weirs under bed level are also possible
 
-         if (((zc > bobL .or. include_fixed_weir_below_bob) .and. zc >= zcrest(L)) .or. ifirstweir(L) == 1) then ! For Villemonte and Tabellenboek fixed weirs under bed level are also possible
+         if (((comparereal(zc,bobL) == 1 .or. include_fixed_weir_below_bob) .and. comparereal(zc,zcrest(L))==1 ) .or. ifirstweir(L) == 1) then ! For Villemonte and Tabellenboek fixed weirs under bed level are also possible
 
             ! Set whether this is the first time that for this link weir values are set:
             ! As a result, only the first fixed weir under the bed level is used
@@ -461,7 +462,7 @@ contains
                !
                ! check whether crestlevel is higher
                !
-               if (zc > zcrest(L)) then
+               if (comparereal(zc, zcrest(L)) == 1) then
                   zcrest(L) = zc
              !! write (msgbuf,'(a,i5,f10.3)') 'Higher crest level: ', L,  zcrest(L); call msg_flush()
                end if
