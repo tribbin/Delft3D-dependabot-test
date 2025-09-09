1216b5d48acb2ce4d157a5a15f74c96b8f221dc2
FlorisBuwaldaDeltares <floris.buwalda@Deltares.nl>
2025-09-09 13:14:52 +0200

    viu and viclu are now DP, removing the need for a single precision links to centers

--- src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/links_to_centers.F90
+++ src/engines_gpl/dflowfm/packages/dflowfm_kernel/src/dflowfm_kernel/compute/links_to_centers.F90
@@ -30,7 +30,7 @@
 !
 !
 module m_links_to_centers
-   use precision, only: dp, sp
+   use precision, only: dp
    use m_flow, only: lnkx, ndkx, kmx, kmxn, ktop, kbot, lbot, ltop
    use m_flowgeom, only: lnx, ln, wcL, ndx
 
@@ -39,20 +39,44 @@ module m_links_to_centers
 
    public :: links_to_centers
 
-   interface links_to_centers
-      module procedure links_to_centers_dp
-      module procedure links_to_centers_sp
-   end interface links_to_centers
 contains
    !> Set flow node value based on flow link values, where vlin is real(kind=dp)
-   subroutine links_to_centers_dp(vnod, vlin)
+   subroutine links_to_centers(vnod, vlin)
       real(kind=dp), intent(in) :: vlin(lnkx)
-      include 'links_to_centers_body.inc'
-   end subroutine links_to_centers_dp
+      real(kind=dp), intent(out), contiguous :: vnod(:)
+      integer :: L, k1, k2, LL, kk, k_start, k_end, k
 
-   subroutine links_to_centers_sp(vnod, vlin)
-      real(kind=sp), intent(in) :: vlin(lnkx)
-      include 'links_to_centers_body.inc'
-   end subroutine links_to_centers_sp
+      vnod = 0.0_dp
+
+      if (kmx == 0) then
+         do L = 1, lnx
+            k1 = ln(1, L); k2 = ln(2, L)
+            vnod(k1) = vnod(k1) + vlin(L) * wcL(1, L)
+            vnod(k2) = vnod(k2) + vlin(L) * wcL(2, L)
+         end do
+      else
+         do LL = 1, lnx
+            do L = Lbot(LL), Ltop(LL)
+               k1 = ln(1, L); k2 = ln(2, L)
+               vnod(k1) = vnod(k1) + vlin(L) * wcL(1, LL)
+               vnod(k2) = vnod(k2) + vlin(L) * wcL(2, LL)
+            end do
+         end do
+
+         !$OMP PARALLEL DO PRIVATE(kk, k_start, k_end, k)
+         do kk = 1, ndx
+            k_start = ktop(kk) + 1
+            k_end = kbot(kk) + kmxn(kk) - 1
+            !$OMP SIMD ASSERT
+            do k = k_start, k_end
+               if (k <= ndkx) then ! Vector/simd code may go out of bounds without explicit check
+                  vnod(k) = vnod(ktop(kk))
+               end if
+            end do
+         end do
+         !$OMP END PARALLEL DO
+      end if
+
+   end subroutine links_to_centers
 
 end module m_links_to_centers
